a440301b7c5d202ea03db6da13b8c9d2fb50a95e
Stephen Scott <stephenjmscott@gmail.com>
2017-12-05 11:44:06 +0000

    added products

--- products/models.py
+++ products/models.py
@@ -1,3 +1,29 @@
+# Create your models here.
+from __future__ import unicode_literals
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=254, default='')
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    image = models.ImageField(upload_to='images')
+    def __str__(self):
+        return self.name
+        
+        
+class Categories(models.Model):
+    name=models.CharField(max_length=50)
+    parent= models.ForeignKey('self', null=True, blank=True)
+    products= models.ManyToManyField(Product, blank=True)
+    
+    def __str__(self):
+        return self.name
+    
+class ProductImage(models.Model):
+    product = models.ForeignKey(Product, related_name='images')
+    image = models.ImageField(upload_to='images')
+    
+    def __str__(self):
+        return str(self.image)
+    
